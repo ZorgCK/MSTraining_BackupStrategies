b9b54efbdb4764d553fd3c006ceeaab5947a69d0
Kümmel Christian <c.kuemmel@microstream.one>
2023-11-07 16:12:01 +0100

    S3 Backup implemented

--- src/main/java/one/microstream/DB.java
+++ src/main/java/one/microstream/DB.java
@@ -5,23 +5,48 @@ import java.util.Optional;
 
 import io.micronaut.core.io.ResourceResolver;
 import io.micronaut.core.io.scan.ClassPathResourceLoader;
-import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
+import one.microstream.afs.aws.s3.types.S3Connector;
+import one.microstream.afs.blobstore.types.BlobStoreFileSystem;
+import one.microstream.afs.nio.types.NioFileSystem;
+import one.microstream.storage.embedded.types.EmbeddedStorageFoundation;
 import one.microstream.storage.embedded.types.EmbeddedStorageManager;
+import one.microstream.storage.types.Storage;
+import one.microstream.storage.types.StorageBackupSetup;
+import one.microstream.storage.types.StorageChannelCountProvider;
+import one.microstream.storage.types.StorageConfiguration;
 
 
 public class DB
 {
 	public static EmbeddedStorageManager	storageManager;
-	public final static DataRoot			root	= new DataRoot();
+	public final static DataRoot			root		= new DataRoot();
+	private static String					bucketname	= "ms-training-backup-example";
 	
 	static
 	{
 		ClassPathResourceLoader loader = new ResourceResolver().getLoader(ClassPathResourceLoader.class).get();
 		Optional<URL> resource = loader.getResource("microstream.xml");
 		
+		BlobStoreFileSystem backupS3Filesystem = BlobStoreFileSystem.New(
+			S3Connector.Caching(S3Utils.getS3Client()));
+		
 		// @formatter:off
-		storageManager = EmbeddedStorageConfiguration.load(resource.get().getPath())
-			.createEmbeddedStorageFoundation()
-			.createEmbeddedStorageManager(root).start();
+		
+		NioFileSystem          fileSystem     = NioFileSystem.New();
+		storageManager = EmbeddedStorageFoundation.New()
+			.setConfiguration(
+				StorageConfiguration.Builder()
+					.setStorageFileProvider(
+						Storage.FileProviderBuilder(fileSystem)
+							.setDirectory(fileSystem.ensureDirectoryPath("foundationStorage"))
+							.createFileProvider()
+					)
+					.setBackupSetup(
+						StorageBackupSetup.New(backupS3Filesystem.ensureDirectoryPath(bucketname)))
+					.setChannelCountProvider(StorageChannelCountProvider.New(4))
+					.createConfiguration()
+			)
+			.setRoot(root)
+			.createEmbeddedStorageManager().start();
 	}
 }
---
+++ src/main/java/one/microstream/S3Utils.java
@@ -0,0 +1,23 @@
+package one.microstream;
+
+import software.amazon.awssdk.auth.credentials.SystemPropertyCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+
+public class S3Utils
+{
+	private static String bucketname = "ms-training-backup-example";
+	
+	public static S3Client getS3Client()
+	{
+		System.setProperty("aws.accessKeyId", "");
+		System.setProperty("aws.secretAccessKey", "");
+		
+		S3Client client =
+			S3Client.builder().region(Region.EU_CENTRAL_1).credentialsProvider(
+				SystemPropertyCredentialsProvider.create()).build();
+		
+		return client;
+	}
+}
