c0ff113349d9ce762f89c37d14e9d7e24fae1fa6
Kümmel Christian <c.kuemmel@microstream.one>
2024-04-04 11:20:24 +0200

    Change AWS credencials to env variables

--- src/main/java/one/microstream/S3Utils.java
+++ src/main/java/one/microstream/S3Utils.java
@@ -1,6 +1,5 @@
 package one.microstream;
 
-import software.amazon.awssdk.auth.credentials.SystemPropertyCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -9,12 +8,11 @@ public class S3Utils
 {
 	public static S3Client getS3Client()
 	{
-		System.setProperty("aws.accessKeyId", "");
-		System.setProperty("aws.secretAccessKey", "");
+//		System.setProperty("aws.accessKeyId", System.getenv().get("AWS_ACCESS_KEY_ID"));
+//		System.setProperty("aws.secretAccessKey", System.getenv().get("AWS_SECRET_ACCESS_KEY"));
 		
 		S3Client client =
-			S3Client.builder().region(Region.EU_CENTRAL_1).credentialsProvider(
-				SystemPropertyCredentialsProvider.create()).build();
+			S3Client.builder().region(Region.EU_CENTRAL_1).build();
 		
 		return client;
 	}
